2cfbff48841f0dc56441b1847f9ead08fa0f5ea2
shalini-9393 <shalinithatikonda73@gmail.com>
2026-03-24 16:05:35 +0530

    Update 03-variables.sh
    Updated dialogue between PERSON1 and PERSON2 and improved variable assignment formatting.

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PERSON1=Trump 
-PERSON2=Putin
+PERSON1=Trump # no space between value and =
+PERSON2=putin 
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
-echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
-echo "$PERSON2:: I am learning Shell. What about you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. Thanks for asking. How are you doing?"
+echo "$PERSON1:: I am doing good. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning shell scripting. It is really interesting. What about you $PERSON1?"
+echo "$PERSON1:: I am preparing for my JE exam. It is really tough, but I am trying my best to crack it."
